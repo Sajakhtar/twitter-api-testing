3891bcfd988ef0e2a7a92a3ee56b0399eeeffaba
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-24 19:32:51 +0400

    seed update

--- topics_seed.rb
+++ topics_seed.rb
@@ -9,4 +9,7 @@ require 'rest-client'
 key = COINMARKETCAP_KEY
 url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=#{key}&limit=200"
 
-# to do
+
+response = RestClient.get url
+results = JSON.parse(response)
+data = results['data']
