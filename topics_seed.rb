61010681ab9d0aa3411a1384e87b65801dbc2c84
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-24 19:34:45 +0400

    seed pseudo code

--- topics_seed.rb
+++ topics_seed.rb
@@ -4,6 +4,8 @@ require 'json'
 require 'open-uri'
 require 'rest-client'
 
+# call coinggecko api coins list
+
 
 
 key = COINMARKETCAP_KEY
@@ -13,3 +15,13 @@ url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_P
 response = RestClient.get url
 results = JSON.parse(response)
 data = results['data']
+
+
+
+
+data.map! do |crypto|
+  # find coingecko coin id
+  # call coingecko api for coin
+    # get url
+  # set cashtag $symbol
+end
