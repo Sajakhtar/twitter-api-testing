de4fc539d82f577ec97ae53e4ca4b5d15202cfdb
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-24 21:17:52 +0400

    update seed3

--- topics_seed.rb
+++ topics_seed.rb
@@ -6,7 +6,7 @@ require 'rest-client'
 
 # call coinggecko api coins list
 coingecko_response = RestClient.get 'https://api.coingecko.com/api/v3/coins/list'
-rcoingecko_results = JSON.parse(coingecko_response)
+coingecko_results = JSON.parse(coingecko_response)
 
 
 key = COINMARKETCAP_KEY
@@ -20,7 +20,10 @@ data = coinmarketcap_results['data']
 
 data.map! do |crypto|
   # find coingecko coin id
+# crypto["coin_id"] = coingecko_results.find { |coin| coin[] == crypto["symbol"] }
   # call coingecko api for coin
     # get url
   # set cashtag $symbol
+  coin_url = "https://api.coingecko.com/api/v3/coins/#{coin_id}"
+  # IMAGE:THUMB, NAME, SYMBOL, DESCRIPTION, LINKS, MARKET_DATE:CURRENT_PRICE:USD
 end
